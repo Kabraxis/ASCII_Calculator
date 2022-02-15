12143db998dc7605e9c67848f386603746a2339b
Kabraxis <46925680+Kabraxis@users.noreply.github.com>
2022-02-16 00:06:07 +0100

    method for addition

--- src/ASCII_Calculator.java
+++ src/ASCII_Calculator.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class ASCII_Calculator {
+    static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String args[]) {
+
+    }
+
+    public static void addition() {
+        int numberInput = 0;
+
+        System.out.println("Enter a number: ");
+        // need to add a loop
+        numberInput += scanner.nextInt();
+    }
+
 }
